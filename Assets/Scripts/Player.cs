6454d3ad4209eaaafcd9cd6d23c14993863c4380
Sakti Harahap <saktiharahap2606@gmail.com>
2023-07-02 12:49:41 +0700

    add script player
    player menjadi script yang turunan dari Physics object yang dimana physics object menyimpan beberapa variabel yang dibutuhkan player seperti gravity dan ground

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : PhysicsObject
+{
+    [SerializeField] private float maxSpeed = 1;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        targetVelocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed,0);
+        
+        // if the player presses "jump" and we are in grounded, set the velocity a ajump power value
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = 10;
+        }
+    }
+}
